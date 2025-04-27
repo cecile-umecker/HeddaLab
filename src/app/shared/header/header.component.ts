72c447c3366e8a0999adab7e882020aadbf7a83d
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-04-27 16:40:42 +0200

    Set up responsive navbar with dropdown and mobile sidenav structure

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  ngAfterViewInit(): void {
+    // Initialiser la sidenav
+    const sidenavElems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenavElems);
+
+    // Initialiser le dropdown
+    const dropdownElems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(dropdownElems, { coverTrigger: false });
+  }
 }
