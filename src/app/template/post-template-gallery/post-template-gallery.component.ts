878f8819108e2b1bfdf81d0484afcf7b06c6106c
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-05-17 16:03:11 +0200

    add gallery template + style

--- src/app/template/post-template-gallery/post-template-gallery.component.ts
+++ src/app/template/post-template-gallery/post-template-gallery.component.ts
@@ -1,10 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewChecked, ElementRef } from '@angular/core';
+import { Post } from '../../models/post.model';
+
+declare var M:any;
 
 @Component({
   selector: 'app-template-post-gallery',
   templateUrl: './post-template-gallery.component.html',
-  styleUrl: './post-template-gallery.component.scss'
+  styleUrls: ['./post-template-gallery.component.scss']
 })
-export class PostTemplateGalleryComponent {
+export class PostTemplateGalleryComponent implements AfterViewChecked {
   @Input() content: string = '';
+  @Input() post!: Post;
+
+  private materialboxInitialized = false;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewChecked(): void {
+    if (!this.materialboxInitialized) {
+      const container = this.el.nativeElement.querySelector('.gallery-container');
+      const images = container?.querySelectorAll('img');
+
+      if (images && images.length > 0) {
+        images.forEach((img: HTMLImageElement) => {
+          img.classList.add('materialboxed');
+        });
+
+        M.Materialbox.init(images);
+        this.materialboxInitialized = true;
+      }
+    }
+  }
 }
--- src/assets/markdown/projet-galerie.md
+++ src/assets/markdown/projet-galerie.md
@@ -1,15 +1,13 @@
-# Galerie test
-
 Ceci est un post de test pour la galerie.
 
 Voici une série d’images pour tester l’affichage en mode galerie.
 
-![Image 1](../images/about-photo.jpg)
-![Image 2](../images/about-photo.jpg)
-![Image 3](../images/about-photo.jpg)
-![Image 4](../images/about-photo.jpg)
-![Image 5](../images/about-photo.jpg)
-![Image 6](../images/about-photo.jpg)
-![Image 7](../images/about-photo.jpg)
-![Image 8](../images/about-photo.jpg)
-![Image 9](../images/about-photo.jpg)
+![Image 1](/assets/images/about-photo.png)
+![Image 2](/assets/images/about-photo.png)
+![Image 3](/assets/images/about-photo.png)
+![Image 4](/assets/images/about-photo.png)
+![Image 5](/assets/images/about-photo.png)
+![Image 6](/assets/images/about-photo.png)
+![Image 7](/assets/images/about-photo.png)
+![Image 8](/assets/images/about-photo.png)
+![Image 9](/assets/images/about-photo.png)
