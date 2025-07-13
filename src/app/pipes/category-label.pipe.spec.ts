11d03c81cc3ca90bcccb6deae5f0e2e81386f85e
cecile-umecker <74008168+cecile-umecker@users.noreply.github.com>
2025-07-13 10:24:15 +0000

    Deploying to gh-pages from @ cecile-umecker/HeddaLab@3d2659c5d6f2d45cfe1c5845cb74e3f912a0a3f9 🚀

--- src/app/pipes/category-label.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CategoryLabelPipe } from './category-label.pipe';
-
-describe('CategoryLabelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CategoryLabelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
